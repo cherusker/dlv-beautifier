1437c0c2a9099dcd874f35e63dcaa7d2b24a5ced
Armin Hasitzka <prince.cherusker@gmail.com>
2018-05-06 18:28:52 +0200

    Improve output + code: - output: add a counter and show "No answer set" if no answer set was found - code: use strip() for repeated char deletion

--- beautifier.py
+++ beautifier.py
@@ -1,15 +1,20 @@
 #!/bin/python3
 
+import re
 import sys
 
 if __name__ == "__main__":
-    lines = sys.stdin.read().split('\n')
+    lines = sys.stdin.read().strip(' \t\r\n').split('\n')
+    as_count = 0
     for line in lines:
         if line == '':
             continue
-        print("Answer set:")
-        items = line.replace('{', '').replace('}', '').replace('\n', '').split(', ')
+        as_count += 1
+        print("Answer set " + str(as_count) + ":")
+        items = line.strip(' \t\r\n{}').split(', ')
         items.sort()
         for item in items:
             if not item.startswith('-'):
                 print('  ' + item.replace('(', ' ( ').replace(')', ' )').replace(',', ', '))
+    if as_count < 1:
+        print("No answer set")
